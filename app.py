a76bb13efa56f2ce2e05dac9da76293d0022fe9a
Hannes Hollo <haholl@taltech.ee>
2024-06-17 16:57:41 +0300

    Added sidepanel and input box

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from article import Article
 from rss_reader import get_rss_feed
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -16,6 +16,9 @@ feed = get_rss_feed(url=url)
 
 # for entry in feed.entries:
 #     print(entry.published)
+
+
+feeds = []
 
 
 def generate_articles(rss_feed):
@@ -56,6 +59,17 @@ articles = generate_articles(feed)
 @app.route('/')
 def index():
     return render_template('news_feed.html', articles=articles)
+
+
+@app.route('/save_value', methods=['POST'])
+def save_value():
+    # Get the value from the form data
+    value = request.form.get('value')
+    # Add the value to the list
+    feeds.append(value)
+    # Return a success response
+    print(feeds)
+    return '', 200
 
 
 # Add more routes here
