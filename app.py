06e45946f7f2bea05ae128ccc2ead3ddaa8c31ac
Hannes Hollo <haholl@taltech.ee>
2024-06-23 18:13:04 +0300

    Added edit and delete buttons, edit button opens a popup.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from feed import Feed
 from feed_types import FeedType
 from generate_articles import generate_articles
 from rss_reader import get_rss_feed
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -14,7 +14,12 @@ app = Flask(__name__)
 # Parse rss feed
 url = "https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss"
 first_feed = Feed(get_rss_feed(url=url), FeedType.News)
-feeds = [first_feed]
+
+
+@app.before_request
+def before_request():
+    if 'feeds' not in g:
+        g.feeds = [first_feed]
 
 
 # Create article objects for home page
@@ -23,7 +28,7 @@ articles = generate_articles(first_feed)
 
 @app.route('/')
 def index():
-    return render_template('news_feed.html', articles=articles, feeds=feeds, big_title=first_feed.title)
+    return render_template('news_feed.html', articles=articles, feeds=g.feeds, big_title=first_feed.title)
 
 
 @app.route('/save_value', methods=['POST'])
@@ -44,7 +49,7 @@ async def view_feed():
     feed_title = request.form.get('feed_title')
     # Find the clicked feed
     clicked_feed = None
-    for feed in feeds:
+    for feed in g.feeds:
         if feed.title == feed_title:
             clicked_feed = feed
 
@@ -55,7 +60,14 @@ async def view_feed():
     else:
         articles = generate_articles(first_feed)
     # Render the template with the new articles
-    return render_template('news_feed.html', articles=articles, feeds=feeds, big_title=feed_title)
+    return render_template('news_feed.html', articles=articles, feeds=g.feeds, big_title=feed_title)
+
+
+@app.route("/delete_feed", methods=['POST'])
+def delete_feed():
+    feed_title = request.form.get('feed_title')
+    g.feeds[:] = [feed for feed in g.feeds if feed.title != feed_title]
+    return render_template('news_feed.html', articles=articles, feeds=g.feeds, big_title=feed_title)
 
 
 def add_feed_to_feeds(feed_to_add):
@@ -65,9 +77,9 @@ def add_feed_to_feeds(feed_to_add):
     :param feed_to_add: A feed object to add to the feeds list.
     :return: True if the feed was added, False otherwise.
     """
-    if any(feed.link == feed_to_add.link for feed in feeds):
+    if any(feed.link == feed_to_add.link for feed in g.feeds):
         return False
-    feeds.append(feed_to_add)
+    g.feeds.append(feed_to_add)
     return True
 
 
